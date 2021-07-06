64cf237a6c73b6509722a2d94aae8b54b9c15981
Jason Tucker <jtucker@users.noreply.github.com>
2021-07-05 21:08:16 -0400

    Add default processing of optiin

--- update-readme.fsx
+++ update-readme.fsx
@@ -16,7 +16,7 @@ let checkins =
 
 let formatCheckins =
     checkins
-    |> Array.map (fun checkin -> sprintf $"| image:{checkin.Photo}[{checkin.Name} - {checkin.Brewery}] | Drinking a *{checkin.Name}* by *{checkin.Brewery}*. It is a *{checkin.Style}* style of beer. | I rated it a *{checkin.Rating}*")
+    |> Array.map (fun checkin -> sprintf $"| image:{checkin.Photo |> Option.defaultWith (fun () -> '')}[{checkin.Name} - {checkin.Brewery}] | Drinking a *{checkin.Name}* by *{checkin.Brewery}*. It is a *{checkin.Style}* style of beer. | I rated it a *{checkin.Rating}*")
     |> Array.reduce (fun i acc -> $"{i}{System.Environment.NewLine}{acc}")
 
 let readme = File.ReadAllText(readmeLocation)

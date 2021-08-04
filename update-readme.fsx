7693fbc97c0d58e609b813ab67e5f4a81f8ce166
jtucker <jtucker@users.noreply.github.com>
2021-08-04 14:52:33 +0000

    fix: adding a blank sample file to fix option

--- update-readme.fsx
+++ update-readme.fsx
@@ -1,4 +1,5 @@
 #r "nuget: FSharp.Data"
+
 open System.IO
 open System.Text.RegularExpressions
 open FSharp.Data
@@ -9,16 +10,16 @@ let readmeLocation = Path.Join(currentDirectory, "README.asciidoc")
 let replaceTextInGroup (currentText: string) replaceText (group: Group) = 
     currentText.Replace(group.Value, $"{System.Environment.NewLine}{replaceText}{System.Environment.NewLine}")
 
-type Untappd = JsonProvider<"untappd/beers.json", ResolutionFolder = __SOURCE_DIRECTORY__>
+type Untappd = JsonProvider<"checkin-sample.json", ResolutionFolder = __SOURCE_DIRECTORY__>
 
-let checkins = 
-    Untappd.GetSamples()
+let checkins fromDirectory= 
+    Untappd.Load(Path.Join(fromDirectory, "untappd/beers.json"))
 
 let defaultPhoto img =
     img |> Option.defaultWith (fun () -> "")
 
 let formatCheckins =
-    checkins
+    checkins currentDirectory
     |> Array.map (fun checkin -> sprintf $"| image:{checkin.Photo |> defaultPhoto}[{checkin.Name} - {checkin.Brewery}] | Drinking a *{checkin.Name}* by *{checkin.Brewery}*. It is a *{checkin.Style}* style of beer. | I rated it a *{checkin.Rating}*")
     |> Array.reduce (fun i acc -> $"{i}{System.Environment.NewLine}{acc}")
 
